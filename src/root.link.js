310442d0cd4e2586c598cc87355d0a79007dbe97
unknown <steef12009@gmail.com>
2021-11-29 05:45:05 +0300

    changes

--- src/root.link.js
+++ src/root.link.js
@@ -1,11 +1,11 @@
 export const RESUME_PATH = './Vivekkumar_Javiya_Resume.pdf';
-export const GITHUB_USERNAME = 'codewithvk';
-export const LINKEDIN_USERNAME = 'vivekkumarjaviya';
-export const FIRST_NAME = 'Vivekkumar';
-export const LAST_NAME = 'Javiya';
-export const ADDRESS = 'Rajkot, Gujrat';
-export const EMAIL_ID = 'work.vivekjaviya@gmail.com';
-export const TWITTER_ID = 'Vk_javiya';
+export const GITHUB_USERNAME = 'MostafaHalabi';
+export const LINKEDIN_USERNAME = '';
+export const FIRST_NAME = 'Mostafa';
+export const LAST_NAME = 'Halabi';
+export const ADDRESS = 'Istanbul, Turkey';
+export const EMAIL_ID = 'mostafa.abdelkader.halabi@gmail.com';
+export const TWITTER_ID = '';
 export const FRONT_PAGE_NAME = 'Mostafa Halabi';
 export const FRONT_PAGE_DESC =
-  'I have a passion for developing user-friendly, accessible and responsive websites. I never stop learning and for me, each new project is another adventure.';
+  'I have a passion for developing user-friendly and responsive websites & mobile Apps. I never stop learning and for me, each new project is another adventure.';
